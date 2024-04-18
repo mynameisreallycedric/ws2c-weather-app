b272a6a06f6a1d7065016f8b7d7d0fe66b8e42ea
Cédric Wagner <cedric.wagner@students.fhnw.ch>
2024-04-18 22:06:50 +0200

    added query

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
 
-const { data, pending, error } = useFetch(() => 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=is_day,rain&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,daylight_duration,rain_sum')
+const { data, pending, error } = useFetch(() => 'https://api.open-meteo.com/v1/forecast', {
+  query: {
+    latitude: 52.52,
+    longitude: 13.41,
+    current: 'is_day,rain',
+    daily: 'weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,daylight_duration,rain_sum'
+  }
+});
 
 </script>
 
